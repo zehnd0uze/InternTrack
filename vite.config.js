bb6689ac93a0ea8bbb184c9230f84baf3cae737d
zehnd0uze <zehnd0uze@users.noreply.github.com>
2026-07-02 10:19:25 +0700

    feat: Add newly designed app logo and fix PWA manifest icons

--- vite.config.js
+++ vite.config.js
@@ -21,14 +21,14 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/favicon.svg',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/svg+xml'
+            type: 'image/png'
           },
           {
-            src: '/favicon.svg',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/svg+xml'
+            type: 'image/png'
           }
         ]
       }
